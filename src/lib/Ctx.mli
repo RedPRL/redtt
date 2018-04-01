83b5fa6bd8ad86419a5cd6aa0e3a53c0a77d082c
Jonathan Sterling <jon@jonmsterling.com>
2018-03-31 22:40:10 -0400

    impose interface on Val; make closures abstract

--- src/lib/Ctx.mli
+++ src/lib/Ctx.mli
@@ -4,6 +4,4 @@ val emp : t
 val define : ctx:t -> ty:Val.can Val.t -> tm:Val.can Val.t -> t
 val add : ctx:t -> ty:Val.can Val.t -> t * Val.can Val.t
 
-val env : ctx:t -> Val.env
-val tys : ctx:t -> Val.env
 val len : ctx:t -> int
---
+++ src/lib/Val.mli
@@ -0,0 +1,39 @@
+(* sorts *)
+type can
+type neu
+
+type 'a bnd = B of 'a
+
+
+type clo
+type bclo
+
+type _ t = 
+  | Idx : int -> can t
+  | Lvl : int -> neu t
+
+  | Up : can t * neu t -> can t
+
+  | Pi : clo * bclo -> can t
+  | Sg : clo * bclo -> can t
+  | Univ : Lvl.t -> can t
+  | Interval : can t
+
+  | Lam : bclo -> can t
+  | Cons : clo * clo -> can t
+
+  | Coe : can t * can t * can t bnd * can t -> can t
+
+  | App : neu t * can t -> neu t
+  | Car : neu t -> neu t
+  | Cdr : neu t -> neu t
+
+
+val eval_clo : clo -> can t
+val inst_bclo : bclo -> can t -> can t
+
+val apply : can t -> can t -> can t
+val car : can t -> can t
+val cdr : can t -> can t
+
+val reflect : can t -> neu t -> can t
