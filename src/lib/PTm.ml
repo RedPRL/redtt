5ca287c7be953d9d4803b145e302b83b32187f52
Jonathan Sterling <jon@jonmsterling.com>
2018-03-31 23:07:24 -0400

    start filling out resolver

--- src/lib/PTm.ml
+++ src/lib/PTm.ml
@@ -11,25 +11,39 @@ type t = Node of {info : info; con : t f}
 
 module type ResEnv =
 sig
+  type 'a el = Var of 'a | Atom of 'a
+
   type t
   val init : t
-  val bind : string -> t -> t
-  val var : string -> t -> int
+  val bind : string el -> t -> t
+  val var : string -> t -> int el
 end
 
 module ResEnv : ResEnv = 
 struct
+  type 'a el = Var of 'a | Atom of 'a
+
   (* TODO: variables and atoms *)
-  type t = string list
+  type t = string el list
 
   let init = []
 
   let bind x env = x :: env
 
-  let rec var x env =
+  let map f el = 
+    match el with 
+    | Var i -> Var (f i)
+    | Atom i -> Atom (f i)
+
+  let proj el = 
+    match el with 
+    | Var i -> i
+    | Atom i -> i
+
+  let rec var x (env : t) =
     match env with 
     | [] -> failwith "variable not found"
-    | y::ys -> if x = y then 0 else 1 + var x ys
+    | y :: ys -> if x = proj y then map (fun _ -> 0) y else map (fun i -> i + 1) @@ var x ys
 end
 
 module Resolver (R : ResEnv) :
@@ -51,13 +65,19 @@ struct
     let Node pf = p in
     match pf.con with
     | Bind (x, p) ->
-      Tm.VB (chk (R.bind x env) p)
+      Tm.VB (chk (R.bind (R.Var x) env) p)
     | _ ->
-      Tm.VB (chk (R.bind "_" env) p)
+      Tm.VB (chk (R.bind (R.Var "_") env) p)
 
-  and inf env p =
+  and inf (env : R.t) p =
     let Node pf = p in
     Tm.into_info pf.info @@
     match pf.con with
+    | Atom x -> 
+      begin
+        match R.var x env with 
+        | R.Atom i -> Tm.Atom i
+        | R.Var i -> Tm.Var i
+      end
     | _ -> failwith ""
 end
