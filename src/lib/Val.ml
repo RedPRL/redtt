312894f291d16d5562465e1a86667020453b48a7
Jonathan Sterling <jon@jonmsterling.com>
2018-04-04 22:23:10 -0400

    use idea from harper-stone to fix extension types

--- src/lib/Val.ml
+++ src/lib/Val.ml
@@ -5,14 +5,14 @@ type 'a bnd = B of 'a
 
 type 'a dimfam = 'a DimFam.t
 
-type _ f = 
+type _ f =
   | Lvl : int -> neu f
 
   | Up : can t * neu t -> can f
 
   | Pi : clo * bclo -> can f
   | Sg : clo * bclo -> can f
-  | Ext : clo * clo system -> can f
+  | Ext : can t * clo system -> can f
   | Univ : Lvl.t -> can f
   | Interval : can f
 
@@ -36,24 +36,34 @@ and 'a tube = DimVal.t * DimVal.t * 'a option
 and 'a system = 'a tube list
 
 and env = can t list
-and clo = [`Eval of Tm.chk Tm.t * env | `Ret of can t] ref
-and bclo = BClo of Tm.chk Tm.t Tm.bnd * env
+and clo = [`Eval of Tm.chk Tm.t * env * frm list | `Ret of can t] ref
+and bclo = BClo of Tm.chk Tm.t Tm.bnd * env * bfrm list
 
-let into vf = 
+and frm =
+  | FApply of can t
+  | FExtCar of clo system
+  | FCar
+  | FCdr
+
+and bfrm =
+  | FExtApp of clo system
+  | FExtCdr of clo system
+
+let into vf =
   {con = vf}
 
 
-let embed_dimval dv = 
-  match dv with 
+let embed_dimval dv =
+  match dv with
   | DimVal.Dim0 -> into Dim0
   | DimVal.Dim1 -> into Dim1
   | DimVal.Lvl i -> into @@ Up (into Interval, into @@ Lvl i)
 
-let out : type a. a t -> a f = 
+let out : type a. a t -> a f =
   fun node -> node.con
 
 
-let project_dimval (type a) (v : a t) = 
+let project_dimval (type a) (v : a t) =
   match out v with
   | Dim0 -> DimVal.Dim0
   | Dim1 -> DimVal.Dim1
@@ -65,36 +75,26 @@ let project_dimval (type a) (v : a t) =
     end
   | _ -> failwith "project_dimval"
 
-let (<:) tm rho =
-  ref @@ `Eval (tm, rho)
+let (<:) tm rho : clo =
+  ref @@ `Eval (tm, rho, [])
 
-let (<:+) bnd rho = 
-  BClo (bnd, rho)
+let (<:+) bnd rho =
+  BClo (bnd, rho, [])
 
 
-let map_tubes f = 
+let map_tubes f =
   List.map @@ fun (vd0, vd1, vbnd) ->
   (vd0, vd1, Option.map f vbnd)
 
 
-let map_btubes f = 
+let map_btubes f =
   map_tubes (DimFam.map f)
 
-
-let out_pi v = 
-  match out v with 
-  | Pi (dom, cod) -> dom, cod
-  | _ -> failwith "out_pi"
-
-let out_sg v = 
-  match out v with 
-  | Sg (dom, cod) -> dom, cod
-  | _ -> failwith "out_sg"
 
 
 let rec eval : type a. env -> a Tm.t -> can t =
   fun rho tm ->
-    match Tm.out tm with 
+    match Tm.out tm with
     | Tm.Var i ->
       let v = Thin.proj i rho in
       v
@@ -106,7 +106,7 @@ let rec eval : type a. env -> a Tm.t -> can t =
       into @@ Sg (dom <: rho, cod <:+ rho)
 
     | Tm.Ext (ty, sys) ->
-      into @@ Ext (ty <: rho, eval_sys rho sys)
+      into @@ Ext (eval rho ty, eval_sys rho sys)
 
     | Tm.Lam bdy ->
       into @@ Lam (bdy <:+ rho)
@@ -124,7 +124,7 @@ let rec eval : type a. env -> a Tm.t -> can t =
     | Tm.HCom (d0, d1, ty, cap, sys) ->
       let vd0 = eval rho d0 in
       let vd1 = eval rho d1 in
-      let vty = eval rho ty in 
+      let vty = eval rho ty in
       let vcap = eval rho cap in
       let vsys = eval_bsys rho sys in
       into @@ HCom (vd0, vd1, vty, vcap, vsys)
@@ -140,7 +140,7 @@ let rec eval : type a. env -> a Tm.t -> can t =
     | Tm.Univ lvl ->
       into @@ Univ lvl
 
-    | Tm.Interval -> 
+    | Tm.Interval ->
       into Interval
 
     | Tm.Dim0 ->
@@ -164,6 +164,22 @@ let rec eval : type a. env -> a Tm.t -> can t =
     | Tm.Up t ->
       eval rho t
 
+and out_pi v =
+  match out v with
+  | Pi (dom, cod) -> dom, cod
+  | Ext (vty, vsys) ->
+    let dom, cod = out_pi vty in
+    dom, wrap_bfrm (FExtApp vsys) cod
+  | _ -> failwith "out_pi"
+
+and out_sg v =
+  match out v with
+  | Sg (dom, cod) -> dom, cod
+  | Ext (vty, vsys) ->
+    let dom, cod = out_sg vty in
+    wrap_frm (FExtCar vsys) dom,
+    wrap_bfrm (FExtCdr vsys) cod
+  | _ -> failwith "out_sg"
 
 and eval_sys rho sys =
   List.map (eval_tube rho) sys
@@ -191,7 +207,7 @@ and eval_btube rho (t0, t1, obnd) =
     | DimVal.Dim0, DimVal.Dim1, _ -> None
     | DimVal.Dim1, DimVal.Dim0, _ -> None
     | _, _, Some (Tm.B tm) ->
-      let vbnd = 
+      let vbnd =
         DimFam.make @@ fun x ->
         eval (embed_dimval x :: rho) tm
       in
@@ -205,26 +221,15 @@ and eval_btube rho (t0, t1, obnd) =
 and com (vd0, vd1, vbnd, vcap, vsys) =
   let vcap' = into @@ Coe (vd0, vd1, vbnd, vcap) in
   let vty' = DimFam.inst vbnd @@ project_dimval vd1 in
-  let tube vbnd = 
+  let tube vbnd =
     DimFam.make @@ fun x ->
     into @@ Coe (embed_dimval x, vd1, vbnd, DimFam.inst vbnd x)
   in
   let vsys' = map_tubes tube vsys in
   into @@ HCom (vd0, vd1, vty', vcap', vsys')
 
-and out_bind_pi vbnd = 
-  DimFam.split @@ DimFam.map out_pi vbnd
-
-and out_bind_sg vbnd = 
-  DimFam.split @@ DimFam.map out_sg vbnd
-
-
-(* TODO: what about when the type-index of coe or hcom is an extension type?
-   I think the right way to figure out what to do in such cases is to first design
-   a version where extension types have explicit introduction and elimination forms.
- *)
-and apply vfun varg = 
-  match out vfun with 
+and apply vfun varg =
+  match out vfun with
   | Lam bclo ->
     inst_bclo bclo varg
 
@@ -234,10 +239,10 @@ and apply vfun varg =
     reflect vcod @@ into @@ App (vneu, varg)
 
   | Coe (vd0, vd1, vbnd, vfun) ->
-    let dom, cod = out_bind_pi vbnd in
+    let dom, cod = DimFam.split @@ DimFam.map out_pi vbnd in
     let vdom = DimFam.map eval_clo dom in
     let vcod =
-      DimFam.make @@ fun x -> 
+      DimFam.make @@ fun x ->
       let coe = into @@ Coe (vd1, embed_dimval x, vdom, varg) in
       inst_bclo (DimFam.inst cod x) coe
     in
@@ -253,25 +258,25 @@ and apply vfun varg =
 
   | _ -> failwith "apply"
 
-and car v = 
-  match out v with 
+and car v =
+  match out v with
   | Cons (clo, _) ->
     eval_clo clo
 
-  | Up (vty, vneu) -> 
-    let dom, cod = out_sg vty in
+  | Up (vty, vneu) ->
+    let dom, _ = out_sg vty in
     let vdom = eval_clo dom in
     reflect vdom @@ into @@ Car vneu
 
   | Coe (vd0, vd1, vbnd, v) ->
-    let dom, cod = out_bind_sg vbnd in
+    let dom, _ = DimFam.split @@ DimFam.map out_sg vbnd in
     let vdom = DimFam.map eval_clo dom in
     let vcar = car v in
     into @@ Coe (vd0, vd1, vdom, vcar)
 
 
   | HCom (vd0, vd1, vty, vcap, vsys) ->
-    let dom, cod = out_sg vty in
+    let dom, _ = out_sg vty in
     let vdom = eval_clo dom in
     let vcap' = car vcap in
     let vsys' = map_btubes car vsys in
@@ -279,8 +284,8 @@ and car v =
 
   | _ -> failwith "car"
 
-and cdr v = 
-  match out v with 
+and cdr v =
+  match out v with
   | Cons (_, clo) ->
     eval_clo clo
 
@@ -290,12 +295,12 @@ and cdr v =
     let vcod = inst_bclo cod vcar in
     reflect vcod @@ into @@ Cdr vneu
 
-  | Coe (vd0, vd1, vbnd, v) -> 
-    let dom, cod = out_bind_pi vbnd in
+  | Coe (vd0, vd1, vbnd, v) ->
+    let dom, cod = DimFam.split @@ DimFam.map out_sg vbnd in
     let vdom = DimFam.map eval_clo dom in
     let vcar = car v in
     let vcod =
-      DimFam.make @@ fun x -> 
+      DimFam.make @@ fun x ->
       let coe = into @@ Coe (vd0, embed_dimval x, vdom, vcar) in
       inst_bclo (DimFam.inst cod x) coe
     in
@@ -304,7 +309,7 @@ and cdr v =
   | HCom (vd0, vd1, vty, vcap, vsys) ->
     let dom, cod = out_sg vty in
     let vdom = eval_clo dom in
-    let vcod = 
+    let vcod =
       DimFam.make @@ fun x ->
       let hcom = into @@ HCom (vd0, embed_dimval x, vdom, car vcap, map_btubes car vsys) in
       inst_bclo cod hcom in
@@ -320,12 +325,12 @@ and reflect vty vneu =
     reflect_ext dom sys vneu
   | _ -> into @@ Up (vty, vneu)
 
-and reflect_ext dom sys vneu = 
-  match sys with 
-  | [] -> reflect (eval_clo dom) vneu
+and reflect_ext dom sys vneu =
+  match sys with
+  | [] -> reflect dom vneu
   | (vd0, vd1, clo) :: sys ->
-    if dim_eq vd0 vd1 then 
-      match clo with 
+    if dim_eq vd0 vd1 then
+      match clo with
       | Some clo -> eval_clo clo
       | None -> failwith "reflect_ext: did not expect None in tube"
     else
@@ -339,14 +344,62 @@ and dim_eq vd0 vd1 =
   | _ -> false
 
 and inst_bclo : bclo -> can t -> can t =
-  fun (BClo (Tm.B tm, vrho)) v ->
-    eval (v :: vrho) tm
+  fun (BClo (Tm.B tm, rho, stk)) varg ->
+    let v = eval (varg :: rho) tm in
+    eval_bstk v stk varg
 
-and eval_clo : clo -> can t = 
+and eval_bstk v bstk varg =
+  match bstk with
+  | [] -> v
+  | bfrm :: bstk ->
+    eval_bfrm bfrm (eval_bstk v bstk varg) varg
+
+and eval_stk v stk =
+  match stk with
+  | [] -> v
+  | frm::stk ->
+    eval_frm frm @@ eval_stk v stk
+
+and eval_frm frm v =
+  match frm with
+  | FExtCar sys ->
+    into @@ Ext (v, map_tubes (wrap_frm FCar) sys)
+
+  | FApply varg ->
+    apply v varg
+
+  | FCar ->
+    car v
+
+  | FCdr ->
+    cdr v
+
+and eval_bfrm bfrm v varg =
+  match bfrm with
+  | FExtApp sys ->
+    into @@ Ext (v, map_tubes (wrap_frm (FApply varg)) sys)
+
+  | FExtCdr sys ->
+    into @@ Ext (v, map_tubes (wrap_frm FCdr) sys)
+
+and eval_clo : clo -> can t =
   fun clo ->
-    match !clo with 
-    | `Eval (tm, rho) ->
-      let v = eval rho tm in
+    match !clo with
+    | `Eval (tm, rho, stk) ->
+      let v = eval_stk (eval rho tm) stk in
       clo := `Ret v;
       v
     | `Ret v -> v
+
+and wrap_frm : frm -> clo -> clo =
+  fun frm clo ->
+    match !clo with
+    | `Eval (tm, rho, stk) ->
+      ref @@ `Eval (tm, rho, frm :: stk)
+    | `Ret v ->
+      ref @@ `Ret (eval_stk v [frm])
+
+and wrap_bfrm : bfrm -> bclo -> bclo =
+  fun bfrm bclo ->
+    let BClo (tm, rho, bstk) = bclo in
+    BClo (tm, rho, bfrm :: bstk)
