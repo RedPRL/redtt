6a5cfd025e73bc60b98da9c9676b9eacd7227824
Jonathan Sterling <jon@jonmsterling.com>
2018-04-09 23:13:26 -0400

    add {rel|irrel} to the typing judgments in preparation for proof irrelevance

--- src/lib/Typing.mli
+++ src/lib/Typing.mli
@@ -1,4 +1,6 @@
 type ctx
 
-val check : ctx:ctx -> ty:Val.can Val.t -> tm:Tm.chk Tm.t -> unit
-val infer : ctx:ctx -> tm:Tm.inf Tm.t -> Val.can Val.t
+type mode = Rel | Irrel
+
+val check : mode:mode -> ctx:ctx -> ty:Val.can Val.t -> tm:Tm.chk Tm.t -> unit
+val infer : mode:mode -> ctx:ctx -> tm:Tm.inf Tm.t -> Val.can Val.t
