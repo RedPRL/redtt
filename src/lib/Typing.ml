2df38b6ed9ed036ce2e56c3c476cde2dbf091bbd
Jonathan Sterling <jon@jonmsterling.com>
2018-04-10 21:55:50 -0400

    restore modes, for a different purpose

--- src/lib/Typing.ml
+++ src/lib/Typing.ml
@@ -42,38 +42,56 @@ struct
 
 end
 
+type mode = FaconDeParler | Real
+
+module Mode =
+struct
+  type t = mode
+
+  let ontologically_real = Real
+  let facon_de_parler = FaconDeParler
+end
+
 type ctx = Ctx.t
 
-let rec check ~ctx ~ty ~tm =
+let rec check ~mode ~ctx ~ty ~tm =
   match Val.out ty, Tm.out tm with
   | Val.Univ lvl, Tm.Univ lvl' ->
     if Lvl.greater lvl lvl' then () else failwith "Universe level failure"
 
   | Val.Univ _, Tm.Pi (dom, Tm.B cod) ->
-    let vdom = check_eval ~ctx ~ty ~tm:dom in
+    let vdom = check_eval ~mode:FaconDeParler ~ctx ~ty ~tm:dom in
     let ctx' = Ctx.ext ctx vdom in
-    check ~ctx:ctx' ~ty ~tm:cod
+    check ~mode:Real ~ctx:ctx' ~ty ~tm:cod
 
   | Val.Univ _, Tm.Sg (dom, Tm.B cod) ->
-    let vdom = check_eval ~ctx ~ty ~tm:dom in
+    let vdom = check_eval ~mode:Real ~ctx ~ty ~tm:dom in
     let ctx' = Ctx.ext ctx vdom in
-    check ~ctx:ctx' ~ty ~tm:cod
+    check ~mode:Real ~ctx:ctx' ~ty ~tm:cod
 
   | Val.Univ _, Tm.Restrict (tag, dom, sys) ->
     failwith "TODO!"
+
+  | Val.Univ _, Tm.Interval tag ->
+    begin
+      match mode with
+      | Real -> failwith "Interval is not ontologically real"
+      | FaconDeParler -> ()
+    end
+
 
   | Val.Pi (dom, cod), Tm.Lam (Tm.B tm) ->
     let vdom = Val.eval_clo dom in
     let ctx' = Ctx.ext ctx vdom in
     let vgen = Val.generic vdom @@ Ctx.len ctx in
     let vcod = Val.inst_bclo cod vgen in
-    check ~ctx:ctx' ~ty:vcod ~tm
+    check ~mode:Real ~ctx:ctx' ~ty:vcod ~tm
 
   | Val.Sg (dom, cod), Tm.Cons (tm0, tm1) ->
     let vdom = Val.eval_clo dom in
-    let vtm0 = check_eval ~ctx ~ty:vdom ~tm:tm0 in
+    let vtm0 = check_eval ~mode:Real ~ctx ~ty:vdom ~tm:tm0 in
     let vcod = Val.inst_bclo cod vtm0 in
-    check ~ctx ~ty:vcod ~tm:tm1
+    check ~mode:Real ~ctx ~ty:vcod ~tm:tm1
 
   | Val.Restrict (tag, dom, sys), _ ->
     failwith "TODO!"
@@ -85,36 +103,36 @@ let rec check ~ctx ~ty ~tm =
     ()
 
   | _, Tm.Up tm ->
-    let ty' = infer ~ctx ~tm in
+    let ty' = infer ~mode ~ctx ~tm in
     let univ = Val.into @@ Val.Univ Lvl.Omega in
     Quote.approx ~ctx:(Ctx.qctx ctx) ~ty:univ ~can0:ty' ~can1:ty
 
   | _ -> failwith ""
 
-and check_eval ~ctx ~ty ~tm =
-  check ~ctx ~ty ~tm;
+and check_eval ~mode ~ctx ~ty ~tm =
+  check ~mode ~ctx ~ty ~tm;
   Val.eval (Ctx.env ctx) tm
 
 
-and infer ~ctx ~tm =
+and infer ~mode ~ctx ~tm =
   match Tm.out tm with
   | Tm.Var th ->
     Ctx.lookup th ctx
 
   | Tm.App (tfun, targ) ->
-    let ty = infer ~ctx ~tm:tfun in
+    let ty = infer ~mode:Real ~ctx ~tm:tfun in
     let dom, cod = Val.out_pi ty in
     let vdom = Val.eval_clo dom in
-    let varg = check_eval ~ctx ~ty:vdom ~tm:targ in
+    let varg = check_eval ~mode:Real ~ctx ~ty:vdom ~tm:targ in
     Val.inst_bclo cod varg
 
   | Tm.Car tm ->
-    let ty = infer ~ctx ~tm in
+    let ty = infer ~mode:Real ~ctx ~tm in
     let dom, _ = Val.out_sg ty in
     Val.eval_clo dom
 
   | Tm.Cdr tm ->
-    let ty = infer ~ctx ~tm in
+    let ty = infer ~mode:Real ~ctx ~tm in
     let _, cod = Val.out_sg ty in
     let vpair = Val.eval (Ctx.env ctx) tm in
     let vcar = Val.car vpair in
@@ -122,19 +140,19 @@ and infer ~ctx ~tm =
 
   | Tm.Down {ty; tm} ->
     let univ = Val.into @@ Val.Univ Lvl.Omega in
-    let vty = check_eval ~ctx ~ty:univ ~tm:ty in
-    check ~ctx ~ty:vty ~tm;
+    let vty = check_eval ~mode:Real ~ctx ~ty:univ ~tm:ty in
+    check ~mode:Real ~ctx ~ty:vty ~tm;
     vty
 
   | Tm.Coe coe ->
     let interval = Val.into @@ Val.Interval coe.tag in
     let univ = Val.into @@ Val.Univ Lvl.Omega in
-    let vdim0 = check_eval ~ctx ~ty:interval ~tm:coe.dim0 in
-    let vdim1 = check_eval ~ctx ~ty:interval ~tm:coe.dim1 in
+    let vdim0 = check_eval ~mode:Real ~ctx ~ty:interval ~tm:coe.dim0 in
+    let vdim1 = check_eval ~mode:Real ~ctx ~ty:interval ~tm:coe.dim1 in
     let Tm.B ty = coe.ty in
-    check ~ctx:(Ctx.ext ctx interval) ~ty:univ ~tm:ty;
+    check ~mode:Real ~ctx:(Ctx.ext ctx interval) ~ty:univ ~tm:ty;
     let vty0 = Val.eval (vdim0 :: Ctx.env ctx) ty in
-    check ~ctx:ctx ~ty:vty0 ~tm:coe.tm;
+    check ~mode:Real ~ctx:ctx ~ty:vty0 ~tm:coe.tm;
     Val.eval (vdim1 :: Ctx.env ctx) ty
 
   | Tm.HCom hcom ->
--- src/lib/Typing.mli
+++ src/lib/Typing.mli
@@ -1,4 +1,20 @@
-type ctx
+module Mode : 
+sig
+  type t
 
-val check : ctx:ctx -> ty:Val.can Val.t -> tm:Tm.chk Tm.t -> unit
-val infer : ctx:ctx -> tm:Tm.inf Tm.t -> Val.can Val.t
+  val facon_de_parler : t
+  val ontologically_real : t
+end
+
+module Ctx :
+sig
+  type t
+  val emp : t
+  val ext : t -> Val.can Val.t -> t
+end
+
+type ctx = Ctx.t
+type mode = Mode.t
+
+val check : mode:mode -> ctx:ctx -> ty:Val.can Val.t -> tm:Tm.chk Tm.t -> unit
+val infer : mode:mode -> ctx:ctx -> tm:Tm.inf Tm.t -> Val.can Val.t
