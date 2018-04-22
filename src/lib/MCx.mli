d537825b0c922bd22380660a4850261cfbccf7cb
Jonathan Sterling <jon@jonmsterling.com>
2018-04-22 16:10:49 -0400

    add some helpful comments

--- src/lib/MCx.mli
+++ src/lib/MCx.mli
@@ -3,11 +3,18 @@
 type t
 type meta = Symbol.t
 
+
+(* In case someone asks, why are the terms and types stored as terms rather than values:
+
+   These terms are used in many different contexts (under explicit substitutions), 
+   and it doesn't make sense to substitute in *value*. But we do know how to
+   take a term and evaluate it under an explicit substitution. *)
+
 type cell = 
   | Ret of Tm.chk Tm.t
   | Ask
 
-type sequent = {lcx : LCx.t; ty : Tm.chk Tm.t; cell : cell}
+type sequent = {lcx : LCx.t; rnv : ResEnv.t; ty : Tm.chk Tm.t; cell : cell}
 
 val emp : t
 val ext : meta -> sequent -> t -> t
