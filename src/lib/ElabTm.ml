98b14575a22c5f4c6d6fb7fad47779d43cb76883
Jonathan Sterling <jon@jonmsterling.com>
2018-04-22 15:40:10 -0400

    start sketching source calculus

---
+++ src/lib/ElabTm.ml
@@ -0,0 +1,19 @@
+(* TODO: this will become the source language,
+   which gets elaborated into the core calculus. *)
+
+
+type chk = Tm.chk
+type inf = Tm.inf
+
+type _ f =
+  | Pi : tele -> chk f
+  | Sg : tele -> chk f
+  | Lam : {vars : string list} -> chk f
+  | Tuple : {cells : chk t list} -> chk f
+  | Hole : chk f
+
+and tele = 
+  | TCons of {vars : string list; dom : chk t; cod : tele}
+  | TEnd of {cod : chk t}
+
+and 'a t = {con : 'a t f}
