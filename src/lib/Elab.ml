7637582de0e064acc94f5eb945f33a3e118f33ef
Jonathan Sterling <jon@jonmsterling.com>
2018-04-22 19:20:12 -0400

    further elaboration

--- src/lib/Elab.ml
+++ src/lib/Elab.ml
@@ -48,14 +48,23 @@ let pi x : hole -> (hole * hole) E.m =
     E.fill alpha tm >>
     E.ret (alpha0, alpha1)
 
-let rec lambdas xs alpha : hole E.m =
-  match xs with
-  | [] -> 
-    E.ret alpha
-
-  | x::xs ->
-    lambda x alpha >>= 
-    lambdas xs
+let sg x : hole -> (hole * hole) E.m = 
+  fun alpha ->
+    E.lookup_goal alpha >>= fun (lcx, rnv, ty) ->
+    let gdom = MCx.{lcx; rnv; ty; cell = Ask} in
+    E.new_goal gdom >>= fun alpha0 ->
+    let tdom = Tm.into @@ Tm.Meta (alpha0, Tm.Id) in
+    E.eval (LCx.env lcx) tdom >>= fun vdom ->
+    let lcx' = LCx.ext lcx vdom in
+    let rnv' = ResEnv.bind x rnv in
+    let gcod = MCx.{lcx = lcx'; rnv = rnv'; ty; cell = Ask} in
+    E.new_goal gcod >>= fun alpha1 ->
+    let tm = 
+      let tcod = Tm.into @@ Tm.Up (Tm.into @@ Tm.Meta (alpha1, Tm.Sub (Tm.Id, Tm.into @@ Tm.Var 0))) in
+      Tm.into @@ Tm.Sg (Tm.into @@ Tm.Up tdom, Tm.B tcod)
+    in
+    E.fill alpha tm >>
+    E.ret (alpha0, alpha1)
 
 let quote rtm alpha : unit E.m = 
   E.lookup_res alpha >>= fun rnv ->
@@ -65,9 +74,63 @@ let quote rtm alpha : unit E.m =
 let rec elab : type a. a ElabTm.t -> hole -> unit E.m =
   fun etm alpha ->
     match ElabTm.out etm with
+    | ElabTm.Pi tele -> 
+      let rec go tele alpha =
+        match tele with
+        | ElabTm.TEnd {cod} -> 
+          elab cod alpha
+
+        | ElabTm.TCons {vars; dom; cod} -> 
+          go_vars vars dom alpha >>=
+          go cod 
+
+      and go_vars vars dom alpha = 
+        match vars with
+        | [] -> 
+          E.ret alpha
+
+        | x::xs ->
+          pi x alpha >>= fun (alpha0, alpha1) ->
+          elab dom alpha0 >>
+          go_vars xs dom alpha1
+
+      in
+      go tele alpha
+
+    | ElabTm.Sg tele -> 
+      let rec go tele alpha =
+        match tele with
+        | ElabTm.TEnd {cod} -> 
+          elab cod alpha
+
+        | ElabTm.TCons {vars; dom; cod} -> 
+          go_vars vars dom alpha >>=
+          go cod 
+
+      and go_vars vars dom alpha = 
+        match vars with
+        | [] -> 
+          E.ret alpha
+
+        | x::xs ->
+          sg x alpha >>= fun (alpha0, alpha1) ->
+          elab dom alpha0 >>
+          go_vars xs dom alpha1
+
+      in
+      go tele alpha
+
     | ElabTm.Lam {vars; bdy} ->
-      lambdas vars alpha >>=
-      elab bdy
+      let rec go xs alpha = 
+        match xs with
+        | [] -> 
+          elab bdy alpha
+
+        | x::xs ->
+          lambda x alpha >>= 
+          go xs
+      in
+      go vars alpha
 
     | ElabTm.Quote rtm ->
       quote rtm alpha
