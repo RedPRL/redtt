f844438831bd7fdbb0e6f34ba8edf558cccbae75
Jonathan Sterling <jon@jonmsterling.com>
2018-05-05 21:39:57 -0400

    experimenting with disjoint-set/union find datastructures

---
+++ src/lib/PersistentTable.mli
@@ -0,0 +1,12 @@
+(* Due to Conchon & Filliatre *)
+
+module type S =
+sig
+  type ('k, 'a) t
+
+  val init : size:int -> ('k, 'a) t
+  val get : 'k -> ('k, 'a) t -> 'a
+  val set : 'k -> 'a -> ('k, 'a) t -> ('k, 'a) t
+end
+
+module M : S
