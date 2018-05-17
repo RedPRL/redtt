27efc0986974a9ba6c9b3fb08def9fa3311b9147
Jon Sterling <jon@jonmsterling.com>
2018-05-17 17:44:21 -0400

    pretty printer for dev ctx (#16)

--- src/lib/Dev.ml
+++ src/lib/Dev.ml
@@ -3,9 +3,9 @@ type tm = Tm.chk Tm.t
 type boundary = Tm.chk Tm.t Tm.system
 
 type cell =
-  | Guess of {ty : ty; guess : dev}
-  | Let of {ty : ty; def : tm}
-  | Lam of ty
+  | Guess of {nm : string option; ty : ty; guess : dev}
+  | Let of {nm : string option; ty : ty; def : tm}
+  | Lam of {nm : string option; ty : ty}
   | Specify of boundary
   | Restrict of {r : Tm.chk Tm.t; r' : Tm.chk Tm.t}
 
@@ -13,6 +13,59 @@ and dev =
   | Hole
   | B of cell * dev
   | Ret of tm
+
+let rec pp_cell env fmt =
+  function
+  | Guess {nm; ty; guess} ->
+    let x, envx = Pretty.Env.bind nm env in
+    begin
+      match guess with
+      | Hole ->
+        Format.fprintf fmt "?%a : %a"
+          Uuseg_string.pp_utf_8 x
+          (Tm.pp env) ty
+      | _ ->
+        Format.fprintf fmt "?%a : %a %a %a"
+          Uuseg_string.pp_utf_8 x
+          (Tm.pp env) ty
+          Uuseg_string.pp_utf_8 "▷"
+          (pp_dev env) guess
+    end;
+    envx
+
+  | Let {nm; ty; def} ->
+    let x, envx = Pretty.Env.bind nm env in
+    Format.fprintf fmt "!%a : %a %a %a"
+      Uuseg_string.pp_utf_8 x
+      (Tm.pp env) ty
+      Uuseg_string.pp_utf_8 "▷"
+      (Tm.pp env) def;
+    envx
+
+  | Lam {nm; ty} ->
+    let x, envx = Pretty.Env.bind nm env in
+    Format.fprintf fmt "λ%a : %a"
+      Uuseg_string.pp_utf_8 x
+      (Tm.pp env) ty;
+    envx
+
+  | Specify _ ->
+    failwith ""
+
+  | Restrict _ ->
+    failwith ""
+
+and pp_dev env fmt =
+  function
+  | Hole ->
+    Format.fprintf fmt "?"
+  | Ret tm ->
+    Format.fprintf fmt "`%a"
+      (Tm.pp env) tm
+  | B (cell, dev) ->
+    let env' = pp_cell env fmt cell in
+    Format.fprintf fmt ". %a"
+      (pp_dev env') dev
 
 (** Development contexts. Following Conor McBride, we define tactics to be admissible rules
     in the theory of valid contexts. *)
@@ -22,12 +75,31 @@ sig
   val emp : t
   val ext : t -> cell -> t
 
+  val pp_cx : t Pretty.t0
+
+  (** Compile a development context to a core-language context, for type checking.
+      Guess binders turn into variable bindings (the guessed term is invisible), just like
+      lambda binders. Let binders turn into definitions (which can be seen in type checking).
+      Restrictions get turned into restrictions. *)
   val core : t -> Typing.cx
 end =
 struct
   type t =
     | Emp
     | Ext of t * cell
+
+  let rec pp_cx_ env fmt =
+    function
+    | Emp -> env
+    | Ext (Emp, c) ->
+      pp_cell env fmt c
+    | Ext (t, c) ->
+      let env' = pp_cx_ env fmt t in
+      Format.fprintf fmt "@,";
+      pp_cell env' fmt c
+
+  let pp_cx fmt cx =
+    ignore @@ pp_cx_ Pretty.Env.emp fmt cx
 
   let emp = Emp
   let ext t c = Ext (t, c)
@@ -41,18 +113,18 @@ struct
       let tcx = core dcx in
       begin
         match c with
-        | Guess {ty; _} ->
+        | Guess {ty; nm; _} ->
           let vty = Typing.Cx.eval tcx ty in
-          fst @@ Typing.Cx.ext_ty tcx ~nm:None vty
+          fst @@ Typing.Cx.ext_ty tcx ~nm vty
 
-        | Let {ty; def} ->
+        | Let {ty; nm; def} ->
           let vty = Typing.Cx.eval tcx ty in
           let vdef = Typing.Cx.eval tcx def in
-          Typing.Cx.def tcx ~nm:None ~ty:vty ~el:vdef
+          Typing.Cx.def tcx ~nm ~ty:vty ~el:vdef
 
-        | Lam ty ->
+        | Lam {ty; nm} ->
           let vty = Typing.Cx.eval tcx ty in
-          fst @@ Typing.Cx.ext_ty tcx ~nm:None vty
+          fst @@ Typing.Cx.ext_ty tcx ~nm vty
 
         | Specify _ ->
           tcx
