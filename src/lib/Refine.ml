5c4cb957203ebfe6ec7ba6293eb169e89ff09fd3
Jonathan Sterling <jon@jonmsterling.com>
2018-05-28 22:56:48 -0400

    idea for how to do proof refinement

--- src/lib/Refine.ml
+++ src/lib/Refine.ml
@@ -1,16 +1,33 @@
-open Unify
-open Contextual
-open RedBasis
-open Bwd
-
+open Unify open Dev open Contextual open RedBasis open Bwd open BwdNotation
 module Notation = Monad.Notation (Contextual)
 open Notation
 
-let pop_hole =
+type goal =
+  {ty : ty;
+   solve : tm -> unit m;
+   subgoal : string -> telescope -> ty -> (tm Tm.cmd -> unit m) -> unit m}
+
+let pop_goal =
   popr >>= function
   | E (alpha, ty, Hole) ->
-    ret (alpha, ty)
+    let gm, cod = telescope ty in
+    begin
+      match Tm.unleash cod with
+      | Tm.LblTy info ->
+        let solve tm = define gm alpha ~ty:cod @@ Tm.make @@ Tm.LblRet tm in
+        let subgoal lbl gm' ty kont =
+          let ty' = Tm.make @@ Tm.LblTy {lbl; args = []; ty} in
+          hole (gm <.> gm') ty' kont
+        in
+        ret {ty = info.ty; solve; subgoal}
+      | _ ->
+        failwith "pop_goal"
+    end
   | _ -> failwith "No hole"
+
+let push_goal lbl gm ty kont =
+  let ty' = Tm.make @@ Tm.LblTy {lbl; args = []; ty} in
+  hole gm ty' kont
 
 let discharge tac =
   tac >>
@@ -20,30 +37,39 @@ let discharge tac =
   | _ -> failwith "Incomplete subgoal"
 
 let refine_pair =
-  pop_hole >>= fun (alpha, ty) ->
-  match Tm.unleash ty with
+  pop_goal >>= fun goal ->
+  match Tm.unleash goal.ty with
   | Tm.Sg (dom, Tm.B (_, cod)) ->
-    hole Emp dom @@ fun proj0 ->
+    goal.subgoal "proj0" Emp dom @@ fun proj0 ->
     let cod' = Tm.subst (Tm.Sub (Tm.Id, proj0)) cod in
-    hole Emp cod' @@ fun proj1 ->
-    define Emp alpha ~ty @@
-    Tm.cons (Tm.up proj0) (Tm.up proj1)
+    goal.subgoal "proj1" Emp cod' @@ fun proj1 ->
+    goal.solve @@ Tm.cons (Tm.up proj0) (Tm.up proj1)
   | _ ->
     failwith "refine_pair"
 
-
 let refine_tt =
-  pop_hole >>= fun (alpha, ty) ->
-  match Tm.unleash ty with
+  pop_goal >>= fun goal ->
+  match Tm.unleash goal.ty with
   | Tm.Bool ->
-    define Emp alpha ~ty @@
-    Tm.make Tm.Tt
+    goal.solve @@ Tm.make Tm.Tt
   | _ ->
     failwith "refine_tt"
 
+
+let refine_lam nm =
+  pop_goal >>= fun goal ->
+  match Tm.unleash goal.ty with
+  | Tm.Pi (dom, cod) ->
+    let x = Name.named @@ Some nm in
+    let codx = Tm.unbind_with x `Only cod in
+    goal.subgoal "body" (Emp #< (x, dom)) codx @@ fun bdyx ->
+    goal.solve @@ Tm.make @@ Tm.Lam (Tm.bind x @@ Tm.up bdyx)
+  | _ ->
+    failwith "refine_lam"
+
 type eterm =
   | Pair of eterm * eterm
-  | Lam of eterm
+  | Lam of string * eterm
   | Tt
 
 let rec elab =
@@ -56,8 +82,9 @@ let rec elab =
     discharge @@ elab e0 >>
     discharge @@ elab e1
 
-  | Lam e ->
-    failwith "No idea how to implement Lam yet; probably need to enrich the proof state zipper"
+  | Lam (x, e) ->
+    refine_lam x >>
+    discharge @@ elab e
 
 let test_script : unit m =
   let alpha = Name.fresh () in
@@ -70,5 +97,3 @@ let test_script : unit m =
     discharge refine_tt
   end >>
   failwith "now, the proof state *should* contain just one entry: a definition of !alpha = (cons tt tt)"
-
-
--- src/lib/Unify.mli
+++ src/lib/Unify.mli
@@ -3,6 +3,7 @@ open Contextual
 open RedBasis.Bwd
 
 type telescope = (Name.t * ty) bwd
+val telescope : ty -> telescope * ty
 val telescope_to_spine : telescope -> tm Tm.spine
 
 val hole : telescope -> ty -> (tm Tm.cmd -> 'a m) -> 'a m
