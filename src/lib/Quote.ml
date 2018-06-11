d16447b37492a29913c265fa51d7b2ff1fd37950
Jonathan Sterling <jon@jonmsterling.com>
2018-06-11 17:33:55 +0200

    some cleanup

--- src/lib/Quote.ml
+++ src/lib/Quote.ml
@@ -339,10 +339,14 @@ struct
 
   and equate_val_abs env ty abs0 abs1 =
     let xs, v0x = Abs.unleash abs0 in
-    let v1x = Abs.inst abs1 @@ List.map Dim.named xs in
-    let envx = Env.abs env xs in
-    let tm = equate envx ty v0x v1x in
-    Tm.B (Name.name @@ List.hd xs, tm)
+    match xs with
+    | [x] ->
+      let v1x = Abs.inst abs1 @@ List.map Dim.named xs in
+      let envx = Env.abs env xs in
+      let tm = equate envx ty v0x v1x in
+      Tm.B (Name.name @@ x, tm)
+    | _ ->
+      failwith "equate_val_abs"
 
   and equate_star env p0 p1 =
     let r0, r'0 = DimStar.unleash p0 in
