0adff58f8be7c88467e7e1c6c0b58a3b04c5f7b7
Jonathan Sterling <jon@jonmsterling.com>
2018-07-01 12:19:09 +0200

    fix doc comment

--- src/frontend/Refiner.mli
+++ src/frontend/Refiner.mli
@@ -6,7 +6,7 @@ type chk_tac = ty -> tm m
 
 exception ChkMatch
 
-(** Try to run a tactic against the current type, but if it raises ChkMatch, re-run it after normalizing the type. *)
+(** Try to run a tactic against the current type, but if it raises [ChkMatch], re-run it after normalizing the type. *)
 val tac_wrap_nf : chk_tac -> chk_tac
 
 
