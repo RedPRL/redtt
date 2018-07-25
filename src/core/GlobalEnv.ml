f4d008cde701e8e4269a84ba8994339f6e2ca06d
Jonathan Sterling <jon@jonmsterling.com>
2018-07-25 14:55:03 +0200

    switch GlobalEnv to ordered representation

--- src/core/GlobalEnv.ml
+++ src/core/GlobalEnv.ml
@@ -1,5 +1,3 @@
-module T = Map.Make (Name)
-
 type 'a param =
   [ `P of 'a
   | `Tw of 'a * 'a
@@ -8,30 +6,37 @@ type 'a param =
 
 type ty = Tm.tm
 type entry = {ty : ty; sys : (Tm.tm, Tm.tm) Tm.system}
-type t = {env : entry param T.t; rel : Restriction.t}
+type t = {env : (Name.t * entry param) list; rel : Restriction.t}
 
 
 let emp () =
-  {env = T.empty;
+  {env = [];
    rel = Restriction.emp ()}
 
 let ext (sg : t) nm param : t =
-  {sg with env = T.add nm param sg.env}
+  {sg with env = (nm, param) :: sg.env}
 
 let define (sg : t) nm ~ty ~tm =
   let face = Tm.make Tm.Dim0, Tm.make Tm.Dim0, Some tm in
   let sys = [face] in
-  {sg with env = T.add nm (`P {ty; sys}) sg.env}
+  {sg with env = (nm, `P {ty; sys}) :: sg.env}
 
+let lookup_entry env nm tw =
+  let rec go =
+    function
+    | [] -> failwith "GlobalEnv.lookup_entry"
+    | (nm', prm) :: env ->
+      if nm' = nm then
+        match prm, tw with
+        | `P a, _ -> a
+        | `Tw (a, _), `TwinL -> a
+        | `Tw (_, a), `TwinR -> a
+        | _ -> failwith "GlobalEnv.lookup_entry"
+      else
+        go env
+  in go env
 
-let lookup_entry sg nm tw =
-  match T.find nm sg, tw with
-  | `P x, _ -> x
-  | `Tw (x, _), `TwinL -> x
-  | `Tw (_, x), `TwinR -> x
-  | _ -> failwith "GlobalEnv.lookup_entry"
-
-let lookup_ty sg nm tw =
+let lookup_ty sg nm (tw : Tm.twin) =
   let {ty; _} = lookup_entry sg.env nm tw in
   ty
 
@@ -47,7 +52,6 @@ let restrict tr0 tr1 sg =
     | _ -> failwith "Restrict: expected dimension"
   in
   let rel', _ = Restriction.equate (ev_dim tr0) (ev_dim tr1) sg.rel in
-  (* Format.eprintf "Restrict: %a ===> %a@." Restriction.pp sg.rel Restriction.pp rel'; *)
   {sg with rel = rel'}
 
 let pp fmt sg =
@@ -61,7 +65,7 @@ let pp fmt sg =
       Format.fprintf fmt "%a[twin]"
         Name.pp nm
   in
-  Format.pp_print_list ~pp_sep go fmt @@ T.bindings sg.env
+  Format.pp_print_list ~pp_sep go fmt @@ List.rev sg.env
 
 let pp_twin fmt =
   function
