6e01d8af9de25df1fe20413c1b034b8f46ce6817
Jonathan Sterling <jon@jonmsterling.com>
2018-08-10 15:57:56 +0200

    TODO comment

--- src/frontend/Log.mli
+++ src/frontend/Log.mli
@@ -4,6 +4,7 @@ type level = [`Info | `Error | `Warn]
 
 type location = (Lexing.position * Lexing.position) option
 
+(* TODO: would be nice to figure out how to turn this into our own printf-style function *)
 val pp_message
   : loc:location
   -> lvl:level
