4a8697663380d204d87826b47ce81575c0482105
Jon Sterling <jon@jonmsterling.com>
2018-08-30 15:03:37 -0400

    start filling on more details

--- src/core/NewDomain.ml
+++ src/core/NewDomain.ml
@@ -48,10 +48,15 @@ module Rel :
 sig
   type t
   val union : dim -> dim -> t -> t
+  val hide : Name.t bwd -> t -> t
+
+  val status : dim -> dim -> t -> [`Equal | `Apart | `Indet]
 end =
 struct
   type t
   let union _ _ _ = failwith ""
+  let hide _ _ = failwith ""
+  let status _ _ _ = failwith ""
 end
 
 (** Permutations *)
@@ -59,10 +64,13 @@ module Perm :
 sig
   type t
   val freshen : Name.t bwd -> Name.t bwd * t
+  val swap_name : t -> Name.t -> Name.t
 end =
 struct
   type t
   let freshen _ = failwith ""
+
+  let swap_name _ = failwith ""
 end
 
 type rel = Rel.t
@@ -75,7 +83,7 @@ sig
   (** [swap] is a purely syntactic operation which perserves the property of being a Ξ-value. *)
   val swap : perm -> t -> t
 
-  (** [subst] is a purely syntactic operation that does {e not} preserve the property of being a Ξ-value; it should usually be followed by [run]. *)
+  (** [subst] is a purely syntactic operation that does {e not} preserve the property of being a Ξ-value; it should usually be followed by [run] with extended Ξ. *)
   val subst : dim -> Name.t -> t -> t
 
   (** [run] brings the element underneath the restriction Ξ. *)
@@ -96,6 +104,14 @@ end =
 struct
   type t = Tm.tm
   let eval _ _ = failwith ""
+end
+
+and Dim : Domain with type t = dim =
+struct
+  type t = dim
+  let swap _ _ = failwith ""
+  let subst _ _ = failwith ""
+  let run _ r = r
 end
 
 and Clo :
@@ -172,28 +188,67 @@ struct
 end
 
 
-and Sys : functor (X : DomainPlug) -> DomainPlug with type t = X.t sys =
+and Sys :
+  functor (X : DomainPlug) ->
+  sig
+    include DomainPlug with type t = X.t sys
+    exception Triv of X.t
+  end =
   functor (X : DomainPlug) ->
   struct
     type t = X.t sys
     module Face = Face (X)
 
-    let swap _ _ = failwith ""
-    let subst _ _ = failwith ""
-    let run _ _ = failwith ""
+    exception Triv of X.t
+
+    let swap pi = List.map @@ Face.swap pi
+    let subst r x = List.map @@ Face.subst r x
+
+    let run rel sys =
+      let run_face face =
+        try [Face.run rel face]
+        with
+        | Face.Dead -> []
+        | Face.Triv bdy -> raise @@ Triv bdy
+      in
+      List.flatten @@ List.map run_face sys
 
     let plug rel frm sys =
       List.map (Face.plug rel frm) sys
   end
 
-and Face : functor (X : DomainPlug) -> DomainPlug with type t = X.t face =
+and Face :
+  functor (X : DomainPlug) ->
+  sig
+    include DomainPlug with type t = X.t face
+
+    exception Triv of X.t
+    exception Dead
+  end =
   functor (X : DomainPlug) ->
   struct
     type t = X.t face
 
-    let swap _ _ = failwith ""
-    let subst _ _ = failwith ""
-    let run _ _ = failwith ""
+    exception Triv of X.t
+    exception Dead
+
+    let swap pi ((r, r'), bdy) =
+      (Dim.swap pi r, Dim.swap pi r'), X.swap pi bdy
+
+    let subst r x ((s, s'), bdy) =
+      (Dim.subst r x s, Dim.subst r x s'), X.subst r x bdy
+
+    let run rel ((r, r'), bdy) =
+      match Rel.status r r' rel with
+      | `Apart ->
+        raise Dead
+      | `Equal ->
+        let bdy' = X.run rel bdy in
+        raise @@ Triv bdy'
+      | `Indet ->
+        let rel' = Rel.union r r' rel in
+        let bdy' = X.run rel' bdy in
+        (r, r'), bdy'
 
     let plug rel frm ((r, r'), bdy) =
       let rel' = Rel.union r r' rel in
@@ -206,9 +261,24 @@ and Abs : functor (X : DomainPlug) -> DomainPlug with type t = X.t abs =
   struct
     type t = X.t abs
 
-    let swap _ _ = failwith ""
-    let subst _ _ = failwith ""
-    let run _ _ = failwith ""
+    let swap pi abs =
+      let Abs (xs, a) = abs in
+      let xs' = Bwd.map (Perm.swap_name pi) xs in
+      let a' = X.swap pi a in
+      Abs (xs', a')
+
+    let subst r z abs =
+      let Abs (xs, a) = abs in
+      let ys, pi = Perm.freshen xs in
+      let a' = X.swap pi a in
+      Abs (ys, a')
+
+    let run rel abs =
+      let Abs (xs, a) = abs in
+      (* TODO: I think the following makes sense, but let's double check. The alternative is to freshen, but it seems like we don't need to if we can un-associate these names. *)
+      let rel' = Rel.hide xs rel in
+      let a' = X.run rel' a in
+      Abs (xs, a')
 
     let plug rel frm abs =
       let Abs (xs, a) = abs in
@@ -218,7 +288,8 @@ and Abs : functor (X : DomainPlug) -> DomainPlug with type t = X.t abs =
         Abs (xs, a')
       | `Might ->
         let ys, pi = Perm.freshen xs in
-        let a' = X.plug rel frm @@ X.swap pi a in
+        let rel' = Rel.hide xs rel in
+        let a' = X.plug rel' frm @@ X.swap pi a in
         Abs (ys, a')
 
   end
