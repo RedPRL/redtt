d4ccc4538795c0a4e6ea6378841f5e0743b9489c
Jon Sterling <jon@jonmsterling.com>
2018-08-30 15:21:35 -0400

    lazy

--- src/core/NewDomain.ml
+++ src/core/NewDomain.ml
@@ -6,7 +6,7 @@ type dim =
   | Dim0
   | Dim1
 
-type 'a face = (dim * dim) * 'a
+type 'a face = (dim * dim) * 'a Lazy.t
 type 'a sys = 'a face list
 type 'a abs = Abs of Name.t bwd * 'a
 
@@ -16,10 +16,10 @@ type 'a abs = Abs of Name.t bwd * 'a
     make this involved in the pre-and-post- conditions of the semantic operations. *)
 type value =
   | Pi of {dom : value; cod : clo}
-  | Neu of {ty : value; neu : neu; sys : value sys}
+  | Sg of {dom : value; cod : clo}
   | Lam of clo
   | Cons of value * value
-  | Up of {neu : neu; sys : value sys}
+  | Neu of {ty : value; neu : neu; sys : value sys}
 
 and head =
   | Lvl of int
@@ -109,18 +109,31 @@ end
 and Dim : Domain with type t = dim =
 struct
   type t = dim
-  let swap _ _ = failwith ""
-  let subst _ _ = failwith ""
+  let swap pi =
+    function
+    | Dim0 | Dim1 as r -> r
+    | Atom x -> Atom (Perm.swap_name pi x)
+
+  let subst r x =
+    function
+    | Dim0 | Dim1 as r -> r
+    | Atom y when x = y -> r
+    | r -> r
+
   let run _ r = r
 end
 
 and Clo :
 sig
-  type t = clo
+  include Domain with type t = clo
   val inst : rel -> t -> cell -> value
 end =
 struct
   type t = clo
+
+  let swap _ _ = failwith ""
+  let subst _ _ = failwith ""
+  let run _ _ = failwith ""
 
   let inst (rel : rel) clo cell : value =
     let Clo {tm; env} = clo in
@@ -133,9 +146,49 @@ struct
 
   module ValSys = Sys (Val)
 
-  let run _ = failwith ""
+  let rec run rel =
+    function
+    | Pi info ->
+      let dom = run rel info.dom in
+      let cod = Clo.run rel info.cod in
+      Pi {dom; cod}
+
+    | Sg info ->
+      let dom = run rel info.dom in
+      let cod = Clo.run rel info.cod in
+      Sg {dom; cod}
+
+    | Neu info ->
+      begin
+        match ValSys.run rel info.sys with
+        | sys ->
+          let neu = Neu.run rel info.neu in
+          let ty = run rel info.ty in
+          Neu {ty; neu; sys}
+        | exception ValSys.Triv v -> v
+      end
+
+    | Lam clo ->
+      let clo = Clo.run rel clo in
+      Lam clo
+
+    | Cons (v0, v1) ->
+      let v0 = run rel v0 in
+      let v1 = run rel v1 in
+      Cons (v0, v1)
+
+
   let swap _ = failwith ""
   let subst _ = failwith ""
+
+  let plug_ty rel frm ty =
+    match ty, frm with
+    | Pi {dom; cod}, FunApp arg ->
+      Clo.inst rel cod @@ Val arg
+    | Sg {dom; _}, Car ->
+      dom
+    | _ ->
+      failwith ""
 
   let plug rel frm hd =
     match frm, hd with
@@ -145,10 +198,11 @@ struct
     | Car, Cons (v0, _) ->
       v0
 
-    | _, Up info ->
+    | _, Neu info ->
       let neu = Neu.plug rel frm info.neu in
       let sys = ValSys.plug rel frm info.sys in
-      Up {neu; sys}
+      let ty = plug_ty rel frm info.ty in
+      Neu {ty; neu; sys}
 
     | _ ->
       failwith ""
@@ -160,7 +214,6 @@ struct
 
   let swap _ = failwith ""
   let run _ = failwith ""
-  let plug _ = failwith ""
   let subst _ = failwith ""
 
   let plug rel frm neu =
@@ -175,9 +228,31 @@ sig
 end =
 struct
   type t = frame
-  let swap _ = failwith ""
-  let subst _ = failwith ""
-  let run _ = failwith ""
+
+  let swap pi =
+    function
+    | FunApp arg ->
+      let arg = Val.swap pi arg in
+      FunApp arg
+    | Car as frm ->
+      frm
+
+  let subst r x =
+    function
+    | FunApp arg ->
+      let arg = Val.subst r x arg in
+      FunApp arg
+    | Car as frm ->
+      frm
+
+
+  let run rel =
+    function
+    | FunApp arg ->
+      let arg = Val.run rel arg in
+      FunApp arg
+    | Car as frm ->
+      frm
 
   let occurs xs =
     function
@@ -233,27 +308,34 @@ and Face :
     exception Dead
 
     let swap pi ((r, r'), bdy) =
-      (Dim.swap pi r, Dim.swap pi r'), X.swap pi bdy
+      (Dim.swap pi r, Dim.swap pi r'),
+      lazy begin X.swap pi @@ Lazy.force bdy end
 
     let subst r x ((s, s'), bdy) =
-      (Dim.subst r x s, Dim.subst r x s'), X.subst r x bdy
+      (Dim.subst r x s, Dim.subst r x s'),
+      lazy begin X.subst r x @@ Lazy.force bdy end
 
     let run rel ((r, r'), bdy) =
       match Rel.status r r' rel with
       | `Apart ->
         raise Dead
       | `Equal ->
-        let bdy' = X.run rel bdy in
+        let bdy' = X.run rel @@ Lazy.force bdy in
         raise @@ Triv bdy'
       | `Indet ->
-        let rel' = Rel.union r r' rel in
-        let bdy' = X.run rel' bdy in
-        (r, r'), bdy'
+        (r, r'),
+        lazy begin
+          let rel' = Rel.union r r' rel in
+          X.run rel' @@ Lazy.force bdy
+        end
 
     let plug rel frm ((r, r'), bdy) =
       let rel' = Rel.union r r' rel in
-      let frm' = Frame.run rel' frm in
-      (r, r'), X.plug rel frm' bdy
+      (r, r'),
+      lazy begin
+        let frm' = Frame.run rel' frm in
+        X.plug rel frm' @@ Lazy.force bdy
+      end
   end
 
 and Abs : functor (X : DomainPlug) -> DomainPlug with type t = X.t abs =
