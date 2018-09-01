006c8911cc586307077ddf67da3e38a24bc32496
Jonathan Sterling <jon@jonmsterling.com>
2018-09-01 09:03:56 -0400

    rejigger to keep old code so that we don't break the whole project

--- src/core/Restriction.ml
+++ src/core/Restriction.ml
@@ -5,10 +5,10 @@ type dim = I.t
 
 type eqn = dim * dim
 
-module IDS = IDisjointSet.Make (PersistentTable.M)
+module UF = DisjointSet.Make (PersistentTable.M)
 
 type t =
-  {classes : IDS.t;
+  {classes : dim UF.t;
    chronicle : eqn list;
    size : int}
 
@@ -24,30 +24,77 @@ let pp fmt rst =
 
 
 let emp () =
-  {classes = IDS.init ~size:100;
+  {classes = UF.init ~size:100;
    chronicle = [];
    size = 0}
 
-let equate r r' t =
+let equate_ r r' t =
   let dl = [r, r'] in
   {chronicle = dl @ t.chronicle;
-   classes = IDS.union r r' t.classes;
+   classes = UF.union r r' t.classes;
    size = t.size + 1}
 
-let compare r r' t = IDS.compare r r' t.classes
+exception Inconsistent = I.Inconsistent
+
+let find r t =
+  try
+    UF.find r t.classes
+  with
+  | _ -> r
+
+let canonize r t =
+  let rr = find r t in
+  let res =
+    if rr = find `Dim0 t then
+      `Dim0
+    else if rr = find `Dim1 t then
+      `Dim1
+    else
+      rr
+  in
+  (* Format.printf "%a |= 0 ==> %a@." pp t D.pp_repr (find D.Dim0 t);
+     Format.printf "Canonizing %a in %a as %a@." D.pp_repr r pp t D.pp_repr res; *)
+  res
+
+let compare r r' t =
+  let cr = canonize r t in
+  let cr' = canonize r' t in
+  I.compare cr cr'
+
+
+let equate r0 r1 t =
+  let res = equate_ r0 r1 t in
+  begin
+    match compare `Dim0 `Dim1 res with
+    | `Same ->
+      raise Inconsistent
+    | _ -> ()
+  end;
+  res, I.equate r0 r1
+
+let as_action t =
+  let rec go =
+    function
+    | [] -> I.idn
+    | (r, r') :: chr ->
+      I.cmp (I.equate r r') (go chr)
+  in
+  go t.chronicle
 
 (* poor man's tests *)
 let _  =
   try
     let x = `Atom (Name.named (Some "i")) in
-    let rst = equate x `Dim1 @@ emp () in
-    let rst = equate x `Dim0 rst in
+    let rst, _ = equate x `Dim1 @@ emp () in
+    let rst, _ = equate x `Dim0 rst in
     Format.printf "Test failure: {@[<1>%a@]}@.\n" pp_chronicle rst.chronicle;
     failwith "Test failed"
   with
-  | I.Inconsistent -> ()
+  | Inconsistent -> ()
 
 let _ =
   let x = `Atom (Name.named (Some "i")) in
-  let rst = equate x `Dim0 @@ emp () in
-  assert (compare x `Dim0 rst == `Same)
+  let rst, _ = equate x `Dim0 @@ emp () in
+  assert (canonize x rst = `Dim0)
+
+
