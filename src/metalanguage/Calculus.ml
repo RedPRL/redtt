a8374b7c9852a1f7772b07b047bcb42b441cee08
Jonathan Sterling <jon@jonmsterling.com>
2018-09-23 16:08:56 -0400

    cleanup and bugfixes

--- src/metalanguage/Calculus.ml
+++ src/metalanguage/Calculus.ml
@@ -1,7 +1,6 @@
 open RedTT_Core
 
-type pol = [`Neg | `Pos]
-type var = Name.t * pol
+type var = [`Neg of Name.t | `Pos of Name.t]
 
 (* A one-sided L calculus. *)
 type term =
@@ -9,7 +8,7 @@ type term =
   | Mu of var * cmd
 
   | Thunk of term
-  | Force of Name.t * cmd
+  | MuThunk of Name.t * cmd
 
   | Tuple of term record
   | Split of Name.t record * cmd
@@ -38,9 +37,9 @@ let pp_tuple pp fmt tuple =
 
 let rec pp_term fmt =
   function
-  | Var (x, _) ->
+  | Var (`Neg x | `Pos x) ->
     Name.pp fmt x
-  | Mu ((x, _), cmd) ->
+  | Mu ((`Neg x | `Pos x), cmd) ->
     Format.fprintf fmt
       "@[<hv1>(%a [%a]@ %a)@]"
       pp_mu ()
@@ -50,9 +49,9 @@ let rec pp_term fmt =
     Format.fprintf fmt
       "@[<hv1>{%a}@]"
       pp_term t
-  | Force (x, cmd) ->
+  | MuThunk (x, cmd) ->
     Format.fprintf fmt
-      "@[<hv1>(force {%a}@ %a)@]"
+      "@[<hv1>(MuThunk {%a}@ %a)@]"
       Name.pp x
       pp_cmd cmd
   | Split (xs, cmd) ->
@@ -90,55 +89,52 @@ and pp_cmd fmt (tm0, tm1) =
 
 module Macro =
 struct
-  let pvar x = Var (x, `Pos)
-  let nvar alpha = Var (alpha, `Neg)
-
   let pmu nm f =
     let x = Name.named @@ Some nm in
-    Mu ((x, `Pos), f x)
+    Mu (`Pos x, f x)
 
   let nmu nm f =
     let x = Name.named @@ Some nm in
-    Mu ((x, `Neg), f x)
+    Mu (`Neg x, f x)
 
   let app_ctx t u =
     let alpha = Name.fresh () in
     let x = Name.fresh () in
-    let pair = Tuple ["in", pvar x; "out", u] in
-    let cmd = t, Mu ((x, `Pos), (nvar alpha, pair)) in
-    Force (alpha, cmd)
+    let pair = Tuple ["in", Var (`Pos x); "out", u] in
+    let cmd = t, Mu (`Pos x, (Var (`Neg alpha), pair)) in
+    MuThunk (alpha, cmd)
 
   let app t u =
     let alpha = Name.fresh () in
-    let cmd = t, app_ctx u (nvar alpha) in
-    Mu ((alpha, `Neg), cmd)
+    let cmd = t, app_ctx u (Var (`Neg alpha)) in
+    Mu (`Neg alpha, cmd)
 
   let lambda nm f =
     let x = Name.named @@ Some nm in
     let alpha = Name.fresh () in
     let binders = ["in", x; "out", alpha] in
-    let cmd = f x, Var (alpha, `Neg) in
+    let cmd = f x, Var (`Neg alpha) in
     Thunk (Split (binders, cmd))
 
   let pair t0 t1 =
     let alpha = Name.fresh () in
     let x = Name.fresh () in
     let y = Name.fresh () in
-    let cmd2 = Tuple ["0", pvar x; "1", pvar y], nvar alpha in
-    let cmd1 = t1, Mu ((y, `Pos), cmd2) in
-    let cmd0 = t0, Mu ((x, `Pos), cmd1) in
-    Mu ((alpha, `Neg), cmd0)
+    let cmd2 = Tuple ["0", Var (`Pos x); "1", Var (`Pos y)], Var (`Neg alpha) in
+    let cmd1 = t1, Mu (`Pos y, cmd2) in
+    let cmd0 = t0, Mu (`Pos x, cmd1) in
+    Mu (`Neg alpha, cmd0)
 end
 
 
 let is_neg_term =
   function
-  | Var (_, `Pos) | Split _ | Force _ | Mu ((_, `Pos), _) | Stop -> true
+  | Var (`Neg _) | Split _ | MuThunk _ | Mu (`Neg _, _) | Stop -> true
   | _ -> false
 
 let is_pos_value =
   function
-  | Var (_, `Pos) | Tuple _ | Thunk _ -> true
+  | Var (`Pos _) | Tuple _ | Thunk _ -> true
   | _ -> false
 
 
@@ -164,11 +160,11 @@ struct
   let step : state -> state =
     fun (Clo (tm0, env0), Clo (tm1, env1)) ->
       match tm0, tm1 with
-      | Mu ((alpha, `Neg), cmd), _ when is_neg_term tm1 ->
+      | Mu (`Neg alpha, cmd), _ when is_neg_term tm1 ->
         let env = Env.add alpha (Clo (tm1, env1)) env0 in
         clo_cmd env cmd
 
-      | _, Mu ((x, `Pos), cmd) when is_pos_value tm0 ->
+      | _, Mu (`Pos x, cmd) when is_pos_value tm0 ->
         let env = Env.add x (Clo (tm0, env0)) env1 in
         clo_cmd env cmd
 
@@ -188,14 +184,14 @@ struct
         let env = List.fold_left alg env0 xs in
         clo_cmd env cmd
 
-      | Thunk term, Force (alpha, cmd) ->
+      | Thunk term, MuThunk (alpha, cmd) ->
         let env = Env.add alpha (Clo (term, env0)) env1 in
         clo_cmd env cmd
 
-      | Var (x, _), _ ->
+      | Var (`Pos x | `Neg x), _ ->
         Env.find x env0, Clo (tm1, env1)
 
-      | _, Var (x, _) ->
+      | _, Var (`Pos x | `Neg x) ->
         Clo (tm0, env0), Env.find x env1
 
       | _ ->
@@ -208,10 +204,15 @@ struct
 
   and subst (env : env) =
     function
-    | Var (x, pol) ->
+    | Var (`Pos x) ->
       begin
         try crush @@ Env.find x env with
-        | Not_found -> Var (x, pol)
+        | Not_found -> Var (`Pos x)
+      end
+    | Var (`Neg alpha) ->
+      begin
+        try crush @@ Env.find alpha env with
+        | Not_found -> Var (`Pos alpha)
       end
     | Tuple tuple ->
       let tuple = List.map (fun (lbl, tm) -> lbl, subst env tm) tuple in
@@ -222,8 +223,8 @@ struct
       Mu (x, subst_cmd env cmd)
     | Thunk tm ->
       Thunk (subst env tm)
-    | Force (x, cmd) ->
-      Force (x, subst_cmd env cmd)
+    | MuThunk (x, cmd) ->
+      MuThunk (x, subst_cmd env cmd)
     | Split (xs, cmd) ->
       Split (xs, subst_cmd env cmd)
     | Choose (lbl, tm) ->
@@ -279,7 +280,7 @@ let example_cmd =
   let f =
     lambda "x" @@ fun x ->
     lambda "y" @@ fun y ->
-    pair (pvar x) (pvar y)
+    pair (Var (`Pos x)) (Var (`Pos y))
   in
   app (app f null) null, Stop
 
