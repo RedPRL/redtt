589fb588f8916be1fe0bf0cf1f9b1ead99fce44b
Jonathan Sterling <jon@jonmsterling.com>
2018-10-09 22:22:43 -0400

    Revert "fix weird bugs"
    This reverts commit 61cc505b9450fdd9522e7b36827b09b22f5c6c7c.

--- src/frontend/Elaborator.ml
+++ src/frontend/Elaborator.ml
@@ -77,8 +77,11 @@ struct
       M.ret `Open
 
   let with_mlenv f k =
+    C.get_mlenv >>= fun env0 ->
     C.modify_mlenv f >>
-    k
+    k >>= fun x ->
+    C.modify_mlenv (fun _ -> env0) >>
+    M.ret x
 
 
   let rec eval_cmd =
@@ -189,7 +192,7 @@ struct
       begin
         eval_val tuple >>= function
         | E.SemTuple vs ->
-          with_mlenv (List.fold_right2 E.Env.set xs vs) @@
+          C.modify_mlenv (List.fold_right2 E.Env.set xs vs) >>
           eval_cmd cmd
         | _ ->
           failwith "expected tuple"
@@ -218,20 +221,7 @@ struct
     | E.MlDataDesc desc -> M.ret @@ E.SemDataDesc desc
     | E.MlTerm tm -> M.ret @@ E.SemTerm tm
     | E.MlSys tm -> M.ret @@ E.SemSys tm
-    | E.MlVar x ->
-      C.get_mlenv <<@> fun env ->
-        begin
-          match E.Env.find x env with
-          | Some v -> v
-          | None ->
-            let pp_mlname fmt =
-              function
-              | `User x -> Format.fprintf fmt "%s" x
-              | `Gen x -> Name.pp fmt x
-            in
-            Format.eprintf "Could not find ML variable: %a@." pp_mlname x;
-            failwith "ML variable not found"
-        end
+    | E.MlVar x -> C.get_mlenv <<@> fun env -> Option.get_exn @@ E.Env.find x env
     | E.MlTuple vs -> traverse eval_val vs <<@> fun rs -> E.SemTuple rs
     | E.MlThunk mlcmd -> C.get_mlenv <<@> fun env -> E.SemThunk (env, mlcmd)
     | E.MlRef nm -> M.ret @@ E.SemRef nm
