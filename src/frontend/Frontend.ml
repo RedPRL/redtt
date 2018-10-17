291df081ecf29437326dcfbe10d3bab5aa8f7586
Jonathan Sterling <jon@jonmsterling.com>
2018-10-17 12:37:11 -0400

    error code

--- src/frontend/Frontend.ml
+++ src/frontend/Frontend.ml
@@ -70,7 +70,7 @@ struct
     with
     | exn ->
       Format.eprintf "@[<v3>Encountered error:@; @[<hov>%a@]@]@." PpExn.pp exn;
-      exit (-1)
+      exit 1
 end
 and Elab : Elaborator.S = Elaborator.Make (Importer)
 
@@ -96,6 +96,7 @@ let load options source =
     let pp fmt () = Format.fprintf fmt "Parse error" in
     Log.pp_message ~loc ~lvl:`Error pp Format.err_formatter ();
     exit 1
+
 
 let load_file options =
   load options `File
