30c30a880f22a64f6dee4d5fb69c10953bcc4a9b
Jon Sterling <jon@jonmsterling.com>
2018-10-18 15:48:41 -0400

    putting in boundary refinements

--- src/core/NewTyping.ml
+++ src/core/NewTyping.ml
@@ -4,7 +4,7 @@ module D = NewDomain
 module Q = NewQuote
 
 type positive = [`Type | `El of D.con | `Dim]
-type classifier = [`Pos of positive | `Neg of D.con]
+type classifier = [`Pos of positive | `Neg of D.con * D.con D.sys]
 
 type error =
   | ExpectedDimension
@@ -22,6 +22,7 @@ sig
   val venv : t -> D.Env.t
   val qenv : t -> Q.QEnv.t
 
+  val extend : t -> D.con -> t * D.con
   val lookup : t -> ?tw:Tm.twin -> int -> classifier
 end
 
@@ -41,6 +42,9 @@ struct
   let lookup cx ?(tw = `Only) ix =
     raise CanJonHelpMe
 
+  let extend _ _ =
+    raise CanJonHelpMe
+
 end
 
 module TC :
@@ -49,20 +53,14 @@ sig
 end =
 struct
 
-  let dispatch ty =
-    match ty with
-    | D.Pi _ | D.Sg _ ->
-      `Neg ty
-    | D.Univ _ | D.Data _ | D.Neu _ ->
-      `Pos (`El ty)
-    | _ ->
-      raise CanJonHelpMe
-
   let eval cx = D.Syn.eval (Cx.rel cx) (Cx.venv cx)
-  let inst_clo cx = D.Clo.inst (Cx.rel cx)
+  let inst_clo cx clo v = D.Clo.inst (Cx.rel cx) clo (D.Val (D.LazyVal.make v))
 
 
   open Tm.Notation
+
+  module ConsSys = D.Sys (D.Con)
+
 
   module Sigma =
   struct
@@ -74,6 +72,12 @@ struct
         Tm.up @@ cmd @< Tm.Fst, Tm.up @@ cmd @< Tm.Snd
       | _ ->
         raise PleaseRaiseProperError
+
+
+    let split_sys cx sys =
+      let sys0 = ConsSys.plug (Cx.rel cx) D.Fst sys in
+      let sys1 = ConsSys.plug (Cx.rel cx) D.Snd sys in
+      sys0, sys1
   end
 
   module Pi =
@@ -90,6 +94,10 @@ struct
         Tm.up @@ cmd' @< frm
       | _ ->
         raise PleaseRaiseProperError
+
+    let sys_body cx sys x =
+      ConsSys.plug (Cx.rel cx) (D.FunApp (D.TypedVal.make (D.Val.make x))) sys
+
   end
 
   let rec check cx (cls : classifier) tm =
@@ -102,25 +110,41 @@ struct
       approx cx pos' pos
 
 
-    | `Neg (D.Sg q), _ ->
+    | `Neg (D.Sg q, sys), _ ->
       let tm0, tm1 = Sigma.split tm in
+      let sys0, sys1 = Sigma.split_sys cx sys in
       let dom = D.Val.unleash q.dom in
-      check_of_ty cx dom tm0;
+      check_of_ty cx dom sys0 tm0;
       let el0 = eval cx tm0 in
-      let cod = inst_clo cx q.cod @@ D.Val (D.LazyVal.make el0) in
-      check_of_ty cx cod tm1
+      let cod = inst_clo cx q.cod el0 in
+      check_of_ty cx cod sys1 tm1
 
-    | `Neg (D.Pi q), _ ->
-      let cx', x = raise CanJonHelpMe in
+    | `Neg (D.Pi q, sys), _ ->
+      let cx', x = Cx.extend cx @@ D.Val.unleash q.dom in
       let bdy = Pi.body tm in
-      let cod = inst_clo cx q.cod @@ D.Val (D.LazyVal.make x) in
-      check_of_ty cx cod bdy
+      let sys' = Pi.sys_body cx sys x in
+      let cod = inst_clo cx q.cod x in
+      check_of_ty cx cod sys' bdy
+
+    | `Neg (D.Ext eclo, sys), _ ->
+      raise CanJonHelpMe
 
     | _ ->
       raise @@ E UnexpectedState
 
-  and check_of_ty cx ty =
-    check cx @@ dispatch ty
+  and check_of_ty cx ty sys tm =
+    match ty with
+    | D.Pi _ | D.Sg _ | D.Ext _ ->
+      check cx (`Neg (ty, sys)) tm
+    | D.Univ _ | D.Data _ | D.Neu _ ->
+      check cx (`Pos (`El ty)) tm;
+      check_boundary cx ty sys @@
+      eval cx tm
+    | _ ->
+      raise CanJonHelpMe
+
+  and check_boundary cx ty sys el =
+    raise CanJonHelpMe
 
   and synth cx cmd : positive =
     raise CanJonHelpMe
